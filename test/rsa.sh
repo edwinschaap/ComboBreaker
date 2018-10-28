6118540694ef47cce436149fbd9aeb7285a68458
Chris van Marle <qistoph@gmail.com>
2018-10-28 17:20:18 +0800

    Remove malfunctioning x931 padding call

---
+++ test/rsa.sh
@@ -0,0 +1,14 @@
+dirname ${BASH_SOURCE[0]}
+
+for i in {0..9}; do
+	#openssl genrsa 2048 > private$i.pem;
+	dd if=/dev/urandom of=data$i.enc bs=256 count=1
+done
+
+PADS=(-oaep -ssl -pkcs)
+
+PAD=${PADS[$RANDOM % ${#PADS[@]} ]}
+echo "PAD: $PAD"
+echo "Some message" | openssl rsautl -inkey private$[ $RANDOM % 10 ].pem $PAD -encrypt > data$[ $RANDOM % 10 ].enc
+
+../combobreaker.py -v -c -z openssl rsautl -inkey [ private*.pem ] -decrypt -in [ data*.enc ] [ -ssl -pkcs -oaep ]
